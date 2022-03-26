38e41e5508ea099351aa4b67f36d30e190241c86
DaiMM <dai.mmachado@gmail.com>
2022-03-26 00:43:05 -0300

    FinalizacaoAula2

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,9 +6,24 @@ function CadastroCategoria (){
     return(
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
-            <Link to="/cadastro/categoria">
-                Cadastrar Categoria
+            
+            <form>
+                <label>
+                    Nome da Categoria:
+                    <input
+                        type="text"
+                    />
+                </label>
+                
+                <button>
+                    Cadastrar
+                </button>
+            </form>
+           
+            <Link to="/">
+                Ir para Home
             </Link>
+
         </PageDefault>
     )
 }
